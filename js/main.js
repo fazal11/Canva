910d0beebc4bdc39ef00f6af5a23f296535a0389
Fazal <fazalfazail732@gmail.com>
2022-03-15 23:19:01 +0500

    update

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:50,
+  nav:true,
+  dots:false,
+  // items:2,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
